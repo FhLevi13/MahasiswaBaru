3212d5101a218f64313e7fa8d17527467a5cb496
FhLevi13 <alifvahlephi46@gmail.com>
2022-09-21 14:34:11 +0700

    Update MainActivity.java

--- app/src/main/java/com/lepii/mahasiswabaru/MainActivity.java
+++ app/src/main/java/com/lepii/mahasiswabaru/MainActivity.java
@@ -3,13 +3,14 @@ package com.lepii.mahasiswabaru;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    EditText etNomorPendaftaran;
-    EditText etNamaCalonMahasiswa;
+    EditText etNomorPendaftaran, etNamaCalonMahasiswa;
     Button btnDaftar;
+    String nomorPendaftaran, namaCalonMahasiswa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +20,19 @@ public class MainActivity extends AppCompatActivity {
         etNomorPendaftaran = findViewById(R.id.et_nomor_pendaftaran);
         etNamaCalonMahasiswa = findViewById(R.id.et_nama_calon_mahasiswa);
         btnDaftar = findViewById(R.id.btn_daftar);
+
+        btnDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+        nomorPendaftaran = etNomorPendaftaran.getText().toString();
+        namaCalonMahasiswa = etNamaCalonMahasiswa.getText().toString();
+
+        if (nomorPendaftaran.trim().equals("")) {
+            etNomorPendaftaran.setError("Mohon isi Nomor Pendaftaran!");
+        }
     }
 }
