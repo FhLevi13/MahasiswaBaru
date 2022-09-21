90c5ef425215aaab5e859ecd5dd040d33c6d8f43
FhLevi13 <alifvahlephi46@gmail.com>
2022-09-21 14:15:26 +0700

    third commit

--- app/src/main/java/com/lepii/mahasiswabaru/MainActivity.java
+++ app/src/main/java/com/lepii/mahasiswabaru/MainActivity.java
@@ -3,12 +3,21 @@ package com.lepii.mahasiswabaru;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etNomorPendaftaran;
+    EditText etNamaCalonMahasiswa;
+    Button btnDaftar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNomorPendaftaran = findViewById(R.id.et_nomor_pendaftaran);
+        etNamaCalonMahasiswa = findViewById(R.id.et_nama_calon_mahasiswa);
+        btnDaftar = findViewById(R.id.btn_daftar);
     }
 }
